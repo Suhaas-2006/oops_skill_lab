0d991093f116a30dab0d52975df122b027d0b4fb
Sai Suhaas N <saisuhaasn@gmail.com>
2025-07-26 11:38:42 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Sum {
+	public static void main(String[]args) {
+int x,y,sum;
+Scanner Sc = new Scanner(System.in);
+System.out.println("Type a number:");
+x=Sc.nextInt();
+System.out.println("Type another number:");
+y=Sc.nextInt();
+sum=x+y;
+System.out.println("Sum is:" + sum);
+	}
+	
+}
+`
---
+++ add.java
@@ -0,0 +1,10 @@
+class add {
+	 public static void main(String args[])
+	 {
+		 int x=10;
+		 int y=30;
+		 int sum=x+y;
+		 System.out.println(sum);
+	 }
+
+}
